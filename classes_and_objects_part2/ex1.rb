33c7401b829355aaf407f5e1a04c7fa20188fdf2
asika Kuo <asika.tw@gmail.com>
2015-03-23 16:01:32 +0800

    ex1

---
+++ classes_and_objects_part2/ex1.rb
@@ -0,0 +1,49 @@
+class MyCar
+	attr_accessor :color
+	attr_reader :year
+
+	def initialize(year, color, model)
+		@year = year
+		@color = color
+		@model = model
+		@cur_spd = 0
+	end
+
+	def self.gas_milage
+		puts "Calculating gas milage"
+	end
+
+	def spd_up
+		@cur_spd += 1
+		puts "current speed="+ @cur_spd.to_s
+	end
+
+	def brake
+		@cur_spd -= 1
+		puts "current speed="+ @cur_spd.to_s
+	end
+
+	def shutoff
+		@cur_spd = 0
+		puts "current speed="+ @cur_spd.to_s
+	end
+
+	def spray_paint(new_color)
+		@color = new_color
+		puts "painting with #{new_color}"
+	end
+end
+
+car = MyCar.new(2015, "yellow", "Tesla-S")
+car.spd_up
+car.spd_up
+car.brake
+car.shutoff
+p car.color
+car.color = "blue"
+p car.color
+p car.year
+#car.year = "1999"
+car.spray_paint("red")
+p car.color
+p car.to_s
